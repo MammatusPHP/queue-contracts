5a98745f97dcfd641158da02bb9314a1c719ed50
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2024-05-08 22:47:20 +0200

    Add message argument to worker perform method

--- src/Worker.php
+++ src/Worker.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Mammatus\Queue\Contracts;
 
+use Interop\Queue\Message;
+
 interface Worker
 {
-    public function perform(): void;
+    public function perform(Message $message): void;
 }
